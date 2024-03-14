7db0e79d2a419344a5bbe662ace9c7ee39d6af7b
Pabitra33 <kumarpabitra345@gmail.com>
2024-03-14 20:54:17 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+//Building a Server that returns Hello World to all user requests.
+
+const express = require('express');
+const port = process.env.PORT || 8000;
+const app = express();
+
+app.use('/', function(req,res){
+    return res.send("<h1>Hello World</h1>");
+})
+
+app.listen(port,function(err){
+    if(err){
+        return console.log('Error:', err);
+    }
+    return console.log('Express Server is up and running on port', port);
+})
